04fdc535036e5f1b7b4a8a56a7ce4d0111aada04
RealEpo <codingonly527@gmail.com>
2026-03-15 22:38:30 +0800

    Fix CORS for Legion Bank

--- server/index.js
+++ server/index.js
@@ -16,17 +16,7 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors({
-  origin: [
-    'https://legion-bank.netlify.app',
-    'https://epo-bank.vercel.app',
-    'http://localhost:5173',
-    'http://localhost:3000'
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
